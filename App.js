c6f6f645d90889fa346efdf3600a85ebab143586
Juan Sebastian <juans.cardenasg@autonoma.edu.co>
2023-08-23 14:09:43 -0500

    orientation en funcionamiento

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeSlide from './src/screens/WelcomeSlide';
-import RegisterForm from './src/screens/RegisterForm';
-import LoginForm from './src/screens/LoginForm'
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Dimensions, StyleSheet } from "react-native";
+import WelcomeSlide from "./src/screens/WelcomeSlide";
+import RegisterForm from "./src/screens/RegisterForm";
+import LoginForm from "./src/screens/LoginForm";
+import { useEffect, useState } from "react";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [orientation, setOrientation] = useState(null);
+
+  const handleOrientationChange = ({ window: { width, height } }) => {
+    const newOrientation = height > width ? "portrait" : "landscape";
+    setOrientation(newOrientation);
+  };
+
+  useEffect(() => {
+    Dimensions.addEventListener("change", handleOrientationChange);
+    return () => {
+      // Dimensions.removeEventListener("change", handleOrientationChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Orientation:", orientation);
+  }, [orientation]); // Se ejecutará cuando cambie la orientación
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="welcome">
-        <Stack.Screen name="Welcome" component={WelcomeSlide}/>
-        <Stack.Screen name="Registro" component={RegisterForm}/>
-        <Stack.Screen name="Login" component={LoginForm}/>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerStyle:
+            orientation === "portrait"
+              ? styles.headerStylePortrait
+              : styles.headerStyleLandscape,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeSlide} />
+        <Stack.Screen name="Registro" component={RegisterForm} />
+        <Stack.Screen name="Login" component={LoginForm} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerStylePortrait: {
+    backgroundColor: "#2181CD",
+    height: 100,
+  },
+  headerStyleLandscape: {
+    backgroundColor: "#2181CD",
+    height: 50,
   },
 });
--- src/screens/WelcomeSlide.js
+++ src/screens/WelcomeSlide.js
@@ -1,9 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
+import Swiper from "react-native-swiper";
 
 const WelcomeSlide = () => {
-
   const navigation = useNavigation();
   const goToRegister = () => {
     navigation.navigate("Registro");
@@ -12,13 +12,52 @@ const WelcomeSlide = () => {
     navigation.navigate("Login");
   };
 
+  const Slide1 = () => {
+    return (
+      <View>
+        <ImageBackground
+          source={require("./images/slide1.png")}
+          style={styles.imgBackground}
+        ></ImageBackground>
+      </View>
+    );
+  };
+  const Slide2 = () => {
+    return (
+      <View>
+        <ImageBackground
+          source={require("./images/slide1.png")}
+          style={styles.imgBackground}
+        ></ImageBackground>
+      </View>
+    );
+  };
+  const Slide3 = () => {
+    return (
+      <View>
+        <ImageBackground
+          source={require("./images/slide1.png")}
+          style={styles.imgBackground}
+        >
+          <Button title="Registrarse" onPress={goToRegister} />
+          <Button title="Iniciar sesión" onPress={goToLogin} />
+        </ImageBackground>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Slides</Text>
-      <Button title="Registrarse" onPress={goToRegister}/>
-      <Button title="Iniciar sesión" onPress={goToLogin}/>
-    </View>
+    <Swiper>
+      <Slide1 />
+      <Slide2 />
+      <Slide3 />
+    </Swiper>
   );
 };
-
+const styles = StyleSheet.create({
+  imgBackground: {
+    width: "100%",
+    height: "90%",
+  },
+});
 export default WelcomeSlide;
